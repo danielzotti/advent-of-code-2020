a2427dcb6b38618a16258370700645f887b19657
Daniel Zotti <daniel.zotti87@gmail.com>
2020-12-02 20:27:54 +0100

    Add day 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.scss';
+import { Day2 } from './Day2/Day2';
 
 function App() {
   return (
     <div className="App">
       <h1>Advent of code</h1>
-
+      <Day2 />
     </div>
   );
 }
---
+++ src/Day2/Day2.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { input1 } from './Day2.inputs';
+import './Day2.scss';
+
+const inputTextRows = input1.split('\n');
+const inputItems = inputTextRows.map(item => {
+  const parts = item.split(' ');
+
+  const [min, max] = parts[0].split('-');
+  const letter = parts[1].substring(0, 1);
+  const text = parts[2];
+
+  return {
+    min: parseInt(min, 10),
+    max: parseInt(max, 10),
+    letter,
+    text
+  } as PasswordItem;
+});
+
+export interface PasswordItem {
+  min: number,
+  max: number,
+  letter: string,
+  text: string
+}
+
+const isPasswordAOk = (item: PasswordItem) => {
+  const { min, max, letter, text } = item;
+  const n = (text.match(new RegExp(letter, 'g')) || []).length;
+  return n >= min && n <= max;
+};
+
+const isPasswordBOk = (item: PasswordItem) => {
+  const { min: posA, max: posB, letter, text } = item;
+  const isPosACorrect = text.charAt(posA - 1) === letter;
+  const isPosBCorrect = text.charAt(posB - 1) === letter;
+
+  return (isPosACorrect && !isPosBCorrect) || (!isPosACorrect && isPosBCorrect);  // XOR
+};
+
+export const Day2: React.FC = () => {
+
+  return (
+    <div>
+      <h1>Day2</h1>
+      <pre>{ inputTextRows.map(item => {
+        return `${ item }
+`;
+      }) }</pre>
+
+      <h2>
+        Day2 A Result: { inputItems.reduce((acc, item) => {
+        return isPasswordAOk(item) ? acc + 1 : acc;
+      }, 0) }
+      </h2>
+      <h2>
+        Day2 B Result: { inputItems.reduce((acc, item) => {
+        return isPasswordBOk(item) ? acc + 1 : acc;
+      }, 0) }
+      </h2>
+    </div>
+  );
+};
